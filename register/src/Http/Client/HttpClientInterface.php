2c76f1da1f61027417e30f1f191824db4770eda3
jorgechavezrnd <jorgechavezrnd@gmail.com>
2023-12-29 11:07:32 -0400

    [012] Update Register Service

---
+++ register/src/Http/Client/HttpClientInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Client;
+
+use Psr\Http\Message\ResponseInterface;
+
+interface HttpClientInterface
+{
+    public function get(string $url, array $options = []): ResponseInterface;
+}
--- register/src/Service/RegisterActionService.php
+++ register/src/Service/RegisterActionService.php
@@ -4,25 +4,43 @@ declare(strict_types=1);
 
 namespace App\Service;
 
+use App\Exception\Http\HttpClientException;
+use App\Http\Client\HttpClientInterface;
 use App\Messenger\Message\UserRegisteredMessage;
 use App\Messenger\RoutingKey;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\ConflictHttpException;
 use Symfony\Component\Messenger\Bridge\Amqp\Transport\AmqpStamp;
 use Symfony\Component\Messenger\MessageBusInterface;
 
 class RegisterActionService
 {
     private MessageBusInterface $bus;
+    private HttpClientInterface $httpClient;
+    private string $applicationServiceUserEndpoint;
 
-    public function __construct(MessageBusInterface $bus)
+    public function __construct(MessageBusInterface $bus, HttpClientInterface $httpClient, string $applicationServiceUserEndpoint)
     {
         $this->bus = $bus;
+        $this->httpClient = $httpClient;
+        $this->applicationServiceUserEndpoint = $applicationServiceUserEndpoint;
     }
 
     public function __invoke(string $name, string $email): void
     {
-        $this->bus->dispatch(
-            new UserRegisteredMessage($name, $email),
-            [new AmqpStamp(RoutingKey::REGISTER_APPLICATION_QUEUE)]
-        );
+        try {
+            $this->httpClient->get(\sprintf('%s?email=%s', $this->applicationServiceUserEndpoint, $email));
+        } catch (HttpClientException $e) {
+            if (JsonResponse::HTTP_NOT_FOUND === $e->getStatusCode()) {
+                $this->bus->dispatch(
+                    new UserRegisteredMessage($name, $email),
+                    [new AmqpStamp(RoutingKey::REGISTER_APPLICATION_QUEUE)]
+                );
+            }
+
+            return;
+        }
+
+        throw new ConflictHttpException(\sprintf('User with email %s already exists', $email));
     }
 }
